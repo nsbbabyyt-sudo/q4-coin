f51464503d121cb3e08383351ca2dbfb92c55b33
nsbbabyyt-sudo <nsb.baby.yt@gmail.com>
2025-10-06 22:34:44 -0500

    chore: remove unused PageClient/ClientShell (fix TS parse error)

--- components/ClientShell.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import dynamic from "next/dynamic";
-
-// Load the actual UI only in the browser (avoids "window is not defined")
-const NoSSRPage = dynamic(() => import("./PageClient"), { ssr: false });
-
-export default function ClientShell() {
-  return <NoSSRPage />;
-}
